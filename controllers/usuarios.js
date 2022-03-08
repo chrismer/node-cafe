76a0302ea1cf8ced043337f106c30c6261cab16e
christian <christianmereles@gmail.com>
2022-03-07 22:33:19 -0300

    primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,59 @@
+const { response, request } = require('express');
+
+
+const usuariosGet = (req = request, res = response) => {
+    
+    const {q, nombre = 'nameless', apikey, page = 1, limit} = req.query;
+
+    res.json({
+        msg: 'get API - Controlador',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+
+    const {id} = req.params;
+
+    res.json({
+        msg: 'put API - Controlador',
+        id
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+
+    // const body = req.body;
+    const {nombre, edad} = req.body;
+
+    res.json({
+        msg: 'post API - Controlador',
+        nombre,
+        edad
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: 'delete API - Controlador'
+    });
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: 'patch API - Controlador'
+    });
+}
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete,
+    usuariosPatch
+}
